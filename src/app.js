62b7192bfd4c37526d2b6a9e0425df16ee5dcf2c
Mariano Luzza <marianoluzza@gmail.com>
2017-09-27 18:35:43 -0300

    UPDATE

--- src/app.js
+++ src/app.js
@@ -12,6 +12,10 @@ import env from './env';
 
 const app = remote.app;
 const appDir = jetpack.cwd(app.getAppPath());
+const imgDir = app.getAppPath() + "\\img";
+
+var request = require('request');
+var fs = require('fs');
 
 // Holy crap! This is browser window with HTML and stuff, but I can read
 // here files form disk like it's node.js! Welcome to Electron world :)
@@ -28,3 +32,86 @@ document.querySelector('#os').innerHTML = osMap[process.platform];
 document.querySelector('#author').innerHTML = manifest.author;
 document.querySelector('#env').innerHTML = env.name;
 document.querySelector('#electron-version').innerHTML = process.versions.electron;
+document.querySelector('#rutaApp').innerHTML = app.getAppPath();
+
+$(".container > div").hide();
+$("#divTest").show();
+
+$("#formInit").submit(function(e){
+  e.preventDefault();
+  return false;
+});
+$("#bQuitTest").click(function () {
+  $("#divTest").hide();
+  $("#divInit").show();
+  $("#ltaGaleria").empty();
+  jetpack.list(imgDir).forEach(function(element) {
+    $('<li/>', {}).html(element).appendTo($('#ltaGaleria'));
+  });
+});
+$("#bIniciar").click(function () {
+  $("#divTest").show();
+  $("#divInit").hide();
+  return false;
+});
+$("#bDescargar").click(function () {
+  $("#bDescargar").prop( "disabled", true );
+  var url = $("#codigo").val();
+  var f = getFilenameFromUrl(url);
+  downloadFile(url, imgDir + "\\" + f);
+  return false;
+});
+$("#bPlay").click(function () {
+  $("#divTest").hide();
+  $("#divVideo").show();
+  $('#divVideo > video').get(0).play()
+  return false;
+});
+function downloadFile(file_url , targetPath){
+  //extraido de https://ourcodeworld.com/articles/read/228/how-to-download-a-webfile-with-electron-save-it-and-show-download-progress
+  // Save variable to know progress
+  var received_bytes = 0;
+  var total_bytes = 0;
+
+  var req = request({
+      method: 'GET',
+      uri: file_url
+  });
+
+  var out = fs.createWriteStream(targetPath);
+  req.pipe(out);
+
+  req.on('response', function ( data ) {
+      // Change the total bytes value to get progress later.
+      total_bytes = parseInt(data.headers['content-length' ]);
+  });
+
+  req.on('data', function(chunk) {
+      // Update the received bytes
+      received_bytes += chunk.length;
+
+      //showProgress(received_bytes, total_bytes);
+  });
+
+  req.on('end', function() {
+    //http://gestioo.com/video.mp4
+    //finalizó la descarga => actualizar la BD    
+    $("#bDescargar").prop( "disabled", false );
+    var video = $('<video />', {      
+      //id: 'video',
+      width: 320,
+      height: 240,
+      src: imgDir + '/video.mp4',
+      type: 'video/mp4',
+      controls: true
+    });
+    video.appendTo($('#divVideo'));
+  });
+}
+function getFilenameFromUrl(url){
+  return url.substring(url.lastIndexOf('/') + 1);
+}
+function showProgress(received,total){
+  var percentage = (received * 100) / total;
+  console.log(percentage + "% | " + received + " bytes out of " + total + " bytes.");
+}
